71307b289fea0e0a63063bf05f7914cde2f050d2
Caffin <caffinmanon@gmail.com>
2024-04-29 19:42:22 +0200

    intégration ok

--- config/routes.php
+++ config/routes.php
@@ -1,22 +1,54 @@
 <?php
-// Exemple de fichier config/routes.php
 
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return function (RoutingConfigurator $routes) {
-    $routes->add('accueil', '/')
+    $routes->add('accueil', '/accueil')
         ->controller('App\Controller\AccueilController::index');
 
-    $routes->add('inscription', '/')
+    $routes->add('inscription', '/inscription')
         ->controller('App\Controller\InscriptionController::index');
 
-    $routes->add('connexion', '/')
+    $routes->add('connexion', '/connexion.html')
         ->controller('App\Controller\ConnexionController::index');
 
     $routes->add('mes_annonces', '/mes-annonces')
         ->controller('App\Controller\MesAnnoncesController::index');
 
-    // Ajoutez d'autres routes au besoin
-};
+    $routes->add('annonce-chercher', '/annonce-chercher')
+        ->controller('App\Controller\AnnonceChercherController::index');
 
-?>
+    $routes->add('annonce-chercher-modifier', '/annonce-chercher-modifier')
+        ->controller('App\Controller\AnnonceChercherModifierController::index');
+
+    $routes->add('annonce-donner', '/annonce-donner')
+        ->controller('App\Controller\AnnonceDonnerController::index');
+
+    $routes->add('annonce-donner-modifier', '/annonce-donner-modifier')
+        ->controller('App\Controller\AnnonceDonnerModifierController::index');
+
+    $routes->add('annonce-detail', '/annonce-detail')
+        ->controller('App\Controller\AnnonceDetail::index');
+
+    $routes->add('annonce-valide', '/annonce-valide')
+        ->controller('App\Controller\AnnonceValideController::index');
+
+    $routes->add('messagerie', '/messagerie')
+        ->controller('App\Controller\MessagerieController::index');
+
+    $routes->add('messagerie-mobile', '/messagerie-mobile')
+        ->controller('App\Controller\MessagerieMobileController::index');
+
+    $routes->add('messages', '/messages')
+        ->controller('App\Controller\MessagesController::index');
+
+    $routes->add('modifications-informations', '/modifications-informations')
+        ->controller('App\Controller\ModificationsInformationsController::index');
+
+    $routes->add('pade-de-confidentialite', '/pade-de-confidentialite')
+        ->controller('App\Controller\PageDeConfidentialiteController::index');
+
+    $routes->add('profil', '/profil')
+        ->controller('App\Controller\ProfilController::index');
+
+};
