4bb8bf162418f79a8a8d024b998cb3b35c2c5e0a
Caffin <caffinmanon@gmail.com>
2024-06-28 19:48:12 +0200

    style research

--- src/Controller/RechercherController.php
+++ src/Controller/RechercherController.php
@@ -6,28 +6,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\ResearchFormType;
+use App\Repository\AnnounceRepository;
+use App\Repository\MaterialRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class RechercherController extends AbstractController
 {
-    #[Route('/rechercher', name: 'app_rechercher')]
-    public function search(Request $request)
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $form = $this->createForm(ResearchFormType::class);
+        $this->entityManager = $entityManager;
+    }
 
-        $form->handleRequest($request);
+    #[Route('/rechercher', name: 'app_rechercher')]
+    public function search(Request $request, EntityManagerInterface $entityManager, AnnounceRepository $announceRepository, MaterialRepository $materialRepository): Response
+    {
+        $researchForm = $this->createForm(ResearchFormType::class);
+        $researchForm->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
+        $materials = $materialRepository->findAll();
+        $announces = [];
 
-            // Récupérez les données du formulaire et effectuez la recherche ici
+        if($researchForm->isSubmitted() && $researchForm->isValid()) {
+            
+            $materialAnnounce = $request->request->all()['material-bio-select'];
+            if(!$materialAnnounce) {
+                $materialAnnounce = $request->request->all()['material-geo-select'];
+            }
 
-            return $this->render('rechercher/resultats.html.twig', [
-                'results' => $results,
-            ]);
+
+            $selectedMaterial = $materialRepository->findOneBy(['material' => $materialAnnounce]);
+            // dd($selectedMaterial);
+            $postalCode = $researchForm->get('geographicalArea')->getData();
+
+            $announces = $announceRepository->findByMaterialAndPostalCode($selectedMaterial, $postalCode);
+            
         }
+
         return $this->render('rechercher/index.html.twig', [
             'controller_name' => 'RechercherController',
-            'form' => $form->createView(),
+            'researchForm' => $researchForm->createView(),
+            'materials' => $materials,
+            'announces' => $announces,
         ]);
     }
 }
--- src/Form/ResearchFormType.php
+++ src/Form/ResearchFormType.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\Announce;
 use App\Entity\ClassificationMaterial;
-use App\Entity\Material;
-use App\Entity\User;
-use App\Entity\Volume;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -19,23 +16,32 @@ class ResearchFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('keyword', TextType::class, [
-                'label' => 'Mot-clé',
-                'required' => false,
-            ])
-            ->add('geographicalArea')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
+            // ->add('keyword', TextType::class, [
+            //     'label' => 'Mot-clé',
+            //     'required' => false,
+            // ])
+
             ->add('classification', EntityType::class, [
                 'class' => ClassificationMaterial::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'placeholder' => '--',
+                'attr' =>
+                [
+                    'onchange' => 'toggleMaterialSelect()',
+                    'id' => 'classification',
+                ],
+                'required' => true,
             ])
-            ->add('material', EntityType::class, [
-                'class' => Material::class,
-                'choice_label' => 'id',
+
+            ->add('geographicalArea', TextType::class, [
+                'label' => 'Localisation',
+                'attr' => [
+                    'placeholder' => 'Entrez le code postal',
+                    'id' => 'search_geographicalArea',
+                ],
+                'required' => true,
             ])
+
             ->add('search', SubmitType::class, [
                 'label' => 'Rechercher',
             ]);
