105a79d2a2dee75bda6eac4c1aa4b515bab248fd
Caffin <caffinmanon@gmail.com>
2024-06-29 16:35:16 +0200

    research form

--- public/js/research-search-city-map.js
+++ public/js/research-search-city-map.js
@@ -1,8 +1,133 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let searchTimeout;
+//     const map = L.map('map').setView([48.8566, 2.3522], 13);
+//     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+//         subdomains: 'abcd',
+//         maxZoom: 19
+//     }).addTo(map);
+
+//     // Déclarer une variable pour stocker la référence au marqueur précédent
+//     let previousMarker = null;
+
+//     document.getElementById("research_form_geographicalArea").addEventListener("input", function () {
+//         const postalCode = this.value.trim();
+
+//         // Déclencher la recherche si le code postal comporte exactement 5 chiffres
+//         if (postalCode.length === 5 && /^\d+$/.test(postalCode)) {
+//             // Annuler le délai précédent pour éviter les requêtes multiples pendant la saisie
+//             clearTimeout(searchTimeout);
+
+//             // Construire l'URL avec des paramètres de recherche spécifiques à la France et aux territoires d'outre-mer
+//             const url = 'https://nominatim.openstreetmap.org/search?format=json&postalcode=' + encodeURIComponent(postalCode) + '&countrycodes=FR,RE,GF,GP,MQ,YT';
+
+//             fetch(url)
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     if (data && data.length > 0) {
+//                         const city = data[0].display_name;
+//                         // Remplir le champ avec la ville trouvée
+//                         document.getElementById("research_form_geographicalArea").value = city;
+//                         // Déclencher la recherche d'adresse automatiquement
+//                         searchAddress();
+//                     } else {
+//                         alert("Aucune ville trouvée pour ce code postal");
+//                     }
+//                 })
+//                 .catch(error => {
+//                     console.error('Erreur lors de la recherche de ville par code postal :', error);
+//                     alert("Une erreur s'est produite lors de la recherche de ville par code postal");
+//                 });
+//         } else if (postalCode.length > 5) {
+//             // Afficher une alerte si le code postal dépasse 5 chiffres
+//             alert("Le code postal doit comporter exactement 5 chiffres.");
+//         }
+//     });
+
+//     function searchAddress() {
+//         const address = document.getElementById("research_form_geographicalArea").value;
+//         // console.log(address)
+//         const announces = JSON.parse(document.getElementById("announces").dataset.announces);
+
+//         const url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(address);
+
+//         fetch(url)
+//             .then(response => response.json())
+//             .then(data => {
+//                 if (data && data.length > 0) {
+//                     const lat = data[0].lat;
+//                     const lon = data[0].lon;
+//                     const myIcon = L.icon({
+//                         iconUrl: '../images/marker.png',
+//                         iconSize: [50, 50],
+//                         iconAnchor: [22, 94],
+//                         popupAnchor: [3, -76],
+//                     });
+
+//                     // Vérifier si des annonces correspondantes ont été trouvées
+//                     const announces = JSON.parse(document.getElementById("announces").dataset.announces);
+//                     // console.log(announces)
+//                     // console.log(announces.length)
+//                     // console.log(typeof announces)
+//                     let hasMatchingAnnounce = false;
+
+//                     // Supprimer tous les marqueurs précédents de la carte
+//                     map.eachLayer(function (layer) {
+//                         if (layer instanceof L.Marker) {
+//                             map.removeLayer(layer);
+//                         }
+//                     });
+
+//                     // Parcourir les annonces pour vérifier s'il y a des correspondances
+//                     // announces.forEach(announce => {
+//                         for (const id in announces) {
+//                             const announce = announces[id];
+
+//                             const marker = L.marker([announce.latitude, announce.longitude], {
+//                                 icon: myIcon
+//                             }).addTo(map);
+
+//                             const popupContent = `
+//                             <div>
+//                                 <h6>${announce.material}</h6>
+//                                 <p>Date: ${announce.createdAt.substring(0, 10)}</p>
+//                                 <p>${announce.description}</p>
+//                                 <a href="./annonce-detail.html"><p>Voir l'annonce</p></a>
+//                             </div> `;
+
+//                             // Ajouter une popup au marqueur pour afficher l'adresse
+//                             marker.bindPopup(popupContent, {
+//                                 className: 'custom-popup',
+//                             });
+
+//                             marker.on('click', function () {
+//                                 this.openPopup();
+//                             });
+//                             hasMatchingAnnounce = true; 
+//                     }
+//                  }
+//                     if (!hasMatchingAnnounce) {
+//                         // Aucune annonce correspondante n'a été trouvée
+//                         alert("Aucune annonce ne correspond à votre recherche.");
+//                     } else {
+//                         map.setView([lat, lon], 13);
+//                     }
+//                 } else {
+//                     alert("Adresse non trouvée");
+//                 }
+//             })
+//             .catch(error => {
+//                 console.error('Erreur lors de la recherche d\'adresse :', error);
+//                 alert("Une erreur s'est produite lors de la recherche d'adresse");
+//             });
+//     }
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
     let searchTimeout;
     const map = L.map('map').setView([48.8566, 2.3522], 13);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
     }).addTo(map);
@@ -46,12 +171,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function searchAddress() {
         const address = document.getElementById("research_form_geographicalArea").value;
-        // console.log(address)
-        const materialBioSelect = document.getElementById("material-bio-select");
-        const materialBio = materialBioSelect ? materialBioSelect.value : "";
+        const materialSelect = document.getElementById("material_select");
+        const material = materialSelect.value;
+        const announces = JSON.parse(document.getElementById("announces").dataset.announces);
 
-        const materialGeoSelect = document.getElementById("material-geo-select");
-        const materialGeo = materialGeoSelect ? materialGeoSelect.value : "";
         const url = 'https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(address);
 
         fetch(url)
@@ -68,10 +191,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     });
 
                     // Vérifier si des annonces correspondantes ont été trouvées
-                    const announces = JSON.parse(document.getElementById("announces").dataset.announces);
-                    // console.log(announces)
-                    // console.log(announces.length)
-                    // console.log(typeof announces)
                     let hasMatchingAnnounce = false;
 
                     // Supprimer tous les marqueurs précédents de la carte
@@ -82,23 +201,23 @@ document.addEventListener("DOMContentLoaded", function () {
                     });
 
                     // Parcourir les annonces pour vérifier s'il y a des correspondances
-                    // announces.forEach(announce => {
-                        for (const id in announces) {
-                            const announce = announces[id];
+                    for (const id in announces) {
+                        const announce = announces[id];
 
-                        if (announce.material === materialBio && announce.geographicalArea === address && announce.type === 'donner') {
+                        if (announce.material === material && announce.geographicalArea === address) {
                             const marker = L.marker([announce.latitude, announce.longitude], {
                                 icon: myIcon
                             }).addTo(map);
 
                             const popupContent = `
-                            <div>
-                                <h6>${announce.material}</h6>
-                                <p>Date: ${announce.createdAt.substring(0, 10)}</p>
-                                <p>${announce.description}</p>
-                                <a href="./annonce-detail.html"><p>Voir l'annonce</p></a>
-                            </div> `;
-
+    
+    <div> 
+    <h6>\${announce.material}</h6>
+    <p>Date: ${announce.createdAt.substring(0, 10)}</p> 
+    <p>${announce.description}</p> 
+    <a href="./annonce-detail.html?id=\${announce.id}">
+    <p>Voir l'annonce</p></a> 
+    </div> `;
                             // Ajouter une popup au marqueur pour afficher l'adresse
                             marker.bindPopup(popupContent, {
                                 className: 'custom-popup',
@@ -107,32 +226,8 @@ document.addEventListener("DOMContentLoaded", function () {
                             marker.on('click', function () {
                                 this.openPopup();
                             });
-                        } else if (announce.material === materialGeo && announce.geographicalArea === address && announce.type === 'donner') {
-                            const marker = L.marker([announce.latitude, announce.longitude], {
-                                icon: myIcon
-                            }).addTo(map);
-
-                            const popupContent = `
-                            <div>
-                                <h6>${announce.material}</h6>
-                                <p>Date: ${announce.createdAt.substring(0, 10)}</p>
-                                <p>${announce.description}</p>
-                                <a href="./annonce-detail.html"><p>Voir l'annonce</p></a>
-                            </div> `;
-
-                            // Ajouter une popup au marqueur pour afficher l'adresse
-                            marker.bindPopup(popupContent, {
-                                className: 'custom-popup',
-                            });
-
-                            marker.on('click', function () {
-                                this.openPopup();
-                            });
-                            
-
                             hasMatchingAnnounce = true;
                         }
-                        
                     }
 
                     if (!hasMatchingAnnounce) {
@@ -146,8 +241,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             })
             .catch(error => {
-                console.error('Erreur lors de la recherche d\'adresse :', error);
-                alert("Une erreur s'est produite lors de la recherche d'adresse");
+                console.error('Erreur lors de la recherche d\'adresse : ', error);
+    alert("Une erreur s'est produite lors de la recherche d'adresse");
             });
     }
 });
--- src/Controller/RechercherController.php
+++ src/Controller/RechercherController.php
@@ -22,7 +22,7 @@ class RechercherController extends AbstractController
     }
 
     #[Route('/rechercher', name: 'app_rechercher')]
-    public function search(Request $request, EntityManagerInterface $entityManager, AnnounceRepository $announceRepository, MaterialRepository $materialRepository): Response
+    public function search(Request $request, AnnounceRepository $announceRepository, MaterialRepository $materialRepository): Response
     {
         $researchForm = $this->createForm(ResearchFormType::class);
         $researchForm->handleRequest($request);
@@ -30,8 +30,12 @@ class RechercherController extends AbstractController
         $materials = $materialRepository->findAll();
         $announces = [];
 
+        $announcesJson = '';
+        $selectedMaterial = null;
         if($researchForm->isSubmitted() && $researchForm->isValid()) {
             
+            $selectedMaterial = $request->request->get('material-bio-select') ?: $request->request->get('material-geo-select');
+            $announcesJson = json_encode($announces);
             $materialAnnounce = $request->request->all()['material-bio-select'];
             if(!$materialAnnounce) {
                 $materialAnnounce = $request->request->all()['material-geo-select'];
@@ -42,15 +46,21 @@ class RechercherController extends AbstractController
             // dd($selectedMaterial);
             $postalCode = $researchForm->get('geographicalArea')->getData();
 
+            // Récupérer les annonces en fonction du matériau sélectionné et du code postal
             $announces = $announceRepository->findByMaterialAndPostalCode($selectedMaterial, $postalCode);
             
+            // Convertir les annonces en tableau JSON et les ajouter à la vue Twig
+            $announcesJson = json_encode($announces);
+            // dump($announces);
         }
 
         return $this->render('rechercher/index.html.twig', [
             'controller_name' => 'RechercherController',
             'researchForm' => $researchForm->createView(),
             'materials' => $materials,
+            'selectedMaterial' => $selectedMaterial,
             'announces' => $announces,
+            'announcesJson' => $announcesJson,
         ]);
     }
 }
